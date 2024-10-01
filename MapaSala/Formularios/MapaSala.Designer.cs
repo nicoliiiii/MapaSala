1bdc93aabb903167631d7236d513844fa31ef2cc
nicoli <santosbuenonicoligabriely@gmail.com>
2024-10-01 10:15:57 -0300

    add mapasala

---
+++ MapaSala/Formularios/MapaSala.Designer.cs
@@ -0,0 +1,218 @@
+﻿
+namespace MapaSala.Formularios
+{
+    partial class MapaSala
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtGridMapaSala = new System.Windows.Forms.DataGridView();
+            this.Segunda = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Terça = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Quarta = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Quinta = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Sexta = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGridMapaSala)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(15, 46);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(147, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(15, 105);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(147, 21);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(12, 159);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(150, 21);
+            this.comboBox3.TabIndex = 3;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(12, 215);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(150, 21);
+            this.comboBox4.TabIndex = 4;
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(12, 278);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(150, 21);
+            this.comboBox5.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 262);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Horario";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 199);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Sala";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Disciplina";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Professores";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Curso";
+            // 
+            // dtGridMapaSala
+            // 
+            this.dtGridMapaSala.BackgroundColor = System.Drawing.Color.Pink;
+            this.dtGridMapaSala.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtGridMapaSala.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Segunda,
+            this.Terça,
+            this.Quarta,
+            this.Quinta,
+            this.Sexta});
+            this.dtGridMapaSala.Location = new System.Drawing.Point(246, 12);
+            this.dtGridMapaSala.Name = "dtGridMapaSala";
+            this.dtGridMapaSala.Size = new System.Drawing.Size(542, 426);
+            this.dtGridMapaSala.TabIndex = 11;
+            // 
+            // Segunda
+            // 
+            this.Segunda.HeaderText = "Segunda";
+            this.Segunda.Name = "Segunda";
+            // 
+            // Terça
+            // 
+            this.Terça.HeaderText = "Terça";
+            this.Terça.Name = "Terça";
+            // 
+            // Quarta
+            // 
+            this.Quarta.HeaderText = "Quarta";
+            this.Quarta.Name = "Quarta";
+            // 
+            // Quinta
+            // 
+            this.Quinta.HeaderText = "Quinta";
+            this.Quinta.Name = "Quinta";
+            // 
+            // Sexta
+            // 
+            this.Sexta.HeaderText = "Sexta";
+            this.Sexta.Name = "Sexta";
+            // 
+            // MapaSala
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dtGridMapaSala);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "MapaSala";
+            this.Text = "MapaSala";
+            ((System.ComponentModel.ISupportInitialize)(this.dtGridMapaSala)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dtGridMapaSala;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Segunda;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Terça;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Quarta;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Quinta;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Sexta;
+    }
+}
