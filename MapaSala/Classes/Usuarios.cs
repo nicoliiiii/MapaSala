5d0ad020f3b52afbd33ff4ce47bebca8155c5e64
nicoli <santosbuenonicoligabriely@gmail.com>
2024-10-22 10:28:31 -0300

    fazendo a classe usuarios

---
+++ MapaSala/Classes/Usuarios.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapaSala.Classes
+{
+   public class Usuarios
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public bool Ativo { get; set; }
+
+        public void Inserir()
+        {
+            Conexao.Open();
+            string query = "insert into Usuarios(Login, Senha, Ativo) Values (@login, @senha,@ativo)";
+
+            SqlCommand comando = new SqlCommand(query, Conexao);
+
+            SqlParameter parametro1 = new SqlParameter("@login", Login);
+            SqlParameter parametro2 = new SqlParameter("@senha", Senha);
+            SqlParameter parametro3 = new SqlParameter("@ativo", Ativo);
+
+            comando.Parameters.Add(parametro1);
+            comando.Parameters.Add(parametro2);
+            comando.Parameters.Add(parametro3);
+            comando.ExecuteNonQuery(); 
+            Conexao.Close();
+        }
+    }
+}
